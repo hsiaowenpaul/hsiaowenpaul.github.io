e740cfeabfe833b8c5d765f8efd591be8a2c4567
hsiaowenpaul <hsiaowenpaul@gmail.com>
2025-10-25 01:31:13 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
     
     setTimeout(() => {
       preloader.classList.add('hidden');
-    }, 2800);
+    }, 3000);
     
     preloader.addEventListener('transitionend', () => {
       preloader.style.display = 'none';
